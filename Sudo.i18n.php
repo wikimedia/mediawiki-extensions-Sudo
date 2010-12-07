b9156ec4d67e8ac4fffd4c5a456995d6801757ca
Raimond Spekking <raymond@users.mediawiki.org>
2010-12-07 21:11:28 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-12-07 20:38:00 UTC)

--- Sudo.i18n.php
+++ Sudo.i18n.php
@@ -51,3 +51,73 @@ $messages['en'] = array(
 	'right-sudo'                  => "Login to another user's account",
 );
 
+/** Message documentation (Message documentation)
+ * @author The Evil IP address
+ */
+$messages['qqq'] = array(
+	'sudo-desc' => '{{desc}}',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ * @author The Evil IP address
+ */
+$messages['de'] = array(
+	'sudo' => 'Mit anderem Benutzerkonto anmelden',
+	'unsudo' => 'Zu deinem eigenen Benutzerkonto zurückkehren',
+	'sudo-desc' => 'Ermöglicht die Anmeldung mit einem anderen Benutzerkonto als dem Eigenen („sudo“)',
+	'sudo-personal-unsudo' => 'Zu deinem eigenen Benutzerkonto zurückkehren',
+	'sudo-form' => 'Anmelden als:',
+	'sudo-user' => 'Benutzername:',
+	'sudo-reason' => 'Grund:',
+	'sudo-submit' => 'Anmelden',
+	'sudo-unsudo' => 'Willkommen $1, du bist momentan als Benutzer „$2“ im Wiki angemeldet. Sende einfach das Formular, um zu deinem normalen Benutzerkonto zurückzukehren.',
+	'sudo-unsudo-submit' => 'Zurückkehren',
+	'sudo-success' => 'Willkommen $1, du bist jetzt im Wiki als Benutzer „$2“ angemeldet.',
+	'sudo-error' => 'Sudo-Fehler: $1',
+	'sudo-error-sudo-invaliduser' => 'Ungültiger Benutzername',
+	'sudo-error-sudo-ip' => 'Man kann sich nicht mit einer IP-Adresse anmelden',
+	'sudo-error-sudo-nonexistent' => 'Dieser Benutzer ist nicht vorhanden',
+	'sudo-error-sudo-self' => 'Man kann sich nicht noch einmal selbst anmelden',
+	'sudo-error-nosudo' => 'Du scheinst nicht mit einem anderen Benutzerkonto angemeldet zu sein',
+	'sudo-logpagename' => 'Sudo-Logbuch',
+	'sudo-logpagetext' => 'Dies ist das Logbuch aller Verwendungen von „sudo“.',
+	'sudo-logentry' => 'hat sich mit dem Benutzerkonto von $2 angemeldet',
+	'right-sudo' => 'Mit anderem Benutzerkonto anmelden',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'sudo' => 'Најава со туѓа сметка',
+	'unsudo' => 'Назад на својата сметка',
+	'sudo-desc' => 'Им овозможува на корисниците на Sudo да се најавуваат со туѓи сметки',
+	'sudo-personal-unsudo' => 'Назад на својата сметка',
+	'sudo-form' => 'Најава на:',
+	'sudo-user' => 'Корисничко име:',
+	'sudo-reason' => 'Причина:',
+	'sudo-submit' => 'Најава',
+	'sudo-unsudo' => 'Добредојдовте, $1. Моментално сте најавени на викито како $2. За да се вратите на вашата редовна сметка, едноставно поднесете го образецот.',
+	'sudo-unsudo-submit' => 'Назад',
+	'sudo-success' => 'Добредојдовте, $1. Сега сте најавени на викито како $2.',
+	'sudo-error' => 'Грешка во Sudo: $1',
+	'sudo-error-sudo-invaliduser' => 'Неважечко корисничко име',
+	'sudo-error-sudo-ip' => 'Не можете да се најавите на IP-адреса',
+	'sudo-error-sudo-nonexistent' => 'Нема таков корисник.',
+	'sudo-error-sudo-self' => 'Не можете да се најавите на својата сметка како на туѓа',
+	'sudo-error-nosudo' => 'Се чини дека не сте во Sudo-сметка',
+	'sudo-logpagename' => 'Дневник на Sudo',
+	'sudo-logpagetext' => 'Ова е дневник на сите употреби на Sudo',
+	'sudo-logentry' => 'најава на сметката на $2',
+	'right-sudo' => 'Најавување на туѓа сметка',
+);
+
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages['te'] = array(
+	'sudo-user' => 'వాడుకరిపేరు',
+	'sudo-reason' => 'కారణం:',
+);
+
