e6b876595aaa3a940405d0460c6fd3e55dedaae0
Jack Phoenix <ashley@uncyclomedia.co>
2018-06-30 00:54:47 +0300

    Extension registration support
    Bumps version from 0.5.0 to 0.6.0.
    MediaWiki 1.29.0+ is now required.
    
    Change-Id: I8ae063695d6e140fbff9eb7faff73410783c06f3

--- Sudo.php
+++ Sudo.php
@@ -14,38 +14,14 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This is an extension to the MediaWiki package and cannot be run standalone.' );
 }
 
-// Display extension properties on MediaWiki.
-$wgExtensionCredits['other'][] = [
-	'path' => __FILE__,
-	'name' => 'Sudo',
-	'version' => '0.5.0',
-	'author' => [
-		'[https://www.mediawiki.org/wiki/User:Dantman Daniel Friesen]',
-		'...'
-	],
-	'descriptionmsg' => 'sudo-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:Sudo',
-	'license-name' => 'GPL-2.0-or-later',
-];
-
-// Register extension messages and other localisation.
-$wgMessagesDirs['Sudo'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['SudoAlias'] = __DIR__ . '/Sudo.alias.php';
-
-// Provide new special page
-$wgAutoloadClasses['SpecialSudo'] = __DIR__ . '/SpecialSudo.php';
-$wgSpecialPages['Sudo'] = 'SpecialSudo';
-
-// New user right, required to use Special:Sudo
-$wgAvailableRights[] = 'sudo';
-
-// New log type, all sudo actions are logged to this log (Special:Log/sudo)
-$wgLogTypes[] = 'sudo';
-$wgLogNames['sudo'] = 'sudo-logpagename';
-$wgLogHeaders['sudo'] = 'sudo-logpagetext';
-$wgLogActions['sudo/sudo'] = 'sudo-logentry';
-
-// Register extension hooks.
-$wgAutoloadClasses['SudoHooks'] = __DIR__ . '/SudoHooks.php';
-$wgHooks['UserLogoutComplete'][] = 'SudoHooks::onUserLogoutComplete';
-$wgHooks['PersonalUrls'][] = 'SudoHooks::onPersonalUrls';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Sudo' );
+	wfWarn(
+		'Deprecated PHP entry point used for Sudo extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the Sudo extension requires MediaWiki 1.29+' );
+}
